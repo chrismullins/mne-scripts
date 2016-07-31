2d10db396154c07b1a36a92fc7b8c45d11d5c255
Christopher Mullins <christopherrmullins@gmail.com>
2016-07-31 15:59:52 -0400

    Fix montage data, add sphere model

--- plot_preproc_topomap_edf.py
+++ plot_preproc_topomap_edf.py
@@ -24,16 +24,19 @@ with open(op.join(datadir, montage_file), 'rt') as csvfile:
     line_num = 0
     for row in spamreader:
         montage_chnames.append(row['labels'].strip('\''))
-        montage_positions[line_num,:] = np.array([np.float(row['X']), np.float(row['Y']), np.float(row['Z'])])
+        # Switch Y and X, otherwise you get frontal sensors on right ear and occipital on left ear
+        montage_positions[line_num,:] = np.array([np.float(row['Y']), np.float(row['X']), np.float(row['Z'])])
         line_num = line_num+1
 tms_montage = mne.channels.Montage(pos=montage_positions, ch_names=montage_chnames, kind='TMS Montage', selection=np.arange(1,62))
+dig_montage = digmontage = mne.channels.DigMontage(hsp=montage_positions, point_names=montage_chnames, hpi=np.zeros((0,3)), elp=np.zeros((0,3)))
 
 # Now read the edf file which is preprocessed by Ryan Downey
 raw_edf = mne.io.read_raw_edf(op.join(datadir, edf), preload=True, stim_channel=62)
 
 # This is probably good to do, although when I use EDF format I need to pass the montage in
 # again later or get the 'no digitization points found' error
-raw_edf.set_montage(tms_montage)
+# Also: figure out the difference between Montage and DigMontage -- we need to use DigMontage
+raw_edf.set_montage(dig_montage)
 
 
 picks = raw_edf.pick_types(meg=False, eeg=True, eog=False, stim=False)
@@ -49,8 +52,10 @@ epochs = mne.Epochs(raw_edf, events_trig_on, event_id, tmin, tmax, proj=True, ba
 
 evoked = epochs.average()
 # Don't normally have to do this for Brainvision
-evoked.set_montage(tms_montage)
+evoked.set_montage(dig_montage)
 #evoked.plot_topomap(times=np.linspace(0.00, 0.5, 10), ch_type='eeg')
+
+sphere_model = mne.bem.make_sphere_model(r0 = 'auto', head_radius = 'auto', info=evoked.info)
 
 #times = np.linspace(tmin, tmax, evoked.data.shape[1])
 #evoked.plot()
@@ -62,5 +67,5 @@ evoked.set_montage(tms_montage)
 ##plt.legend() # legend looks kinda bad
 #plt.show()
 
-#evoked.animate_topomap(ch_type='eeg', times=np.arange(0.05,0.15,0.01), frame_rate=10)
+#fig, anim = evoked.animate_topomap(ch_type='eeg', times=np.linspace(0.00, 0.5, 10), frame_rate=10, show=False)
 
